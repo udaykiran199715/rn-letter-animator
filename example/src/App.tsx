69f87a49ca95b3ed9d787012298bebc28a565a38
udaykiran199715 <udaykiran199715@gmail.com>
2026-07-15 10:57:10 +0530

    test: integrate library into expo example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,41 @@
 import { Text, View, StyleSheet } from 'react-native';
+import { LetterAnimator } from 'rn-letter-animator';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Result:</Text>
+      <View>
+        <View
+          style={{
+            height: 40,
+            width: 300,
+            marginBottom: 24,
+          }}
+        >
+          <LetterAnimator text="Hello World" />
+        </View>
+
+        <View
+          style={{
+            height: 40,
+            width: 300,
+            marginBottom: 24,
+          }}
+        >
+          <LetterAnimator text="👋 Hello 😊" />
+        </View>
+
+        <View
+          style={{
+            height: 40,
+            width: 300,
+            marginBottom: 24,
+          }}
+        >
+          <LetterAnimator text="React Native" />
+        </View>
+      </View>
     </View>
   );
 }
